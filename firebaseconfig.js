6aec9e1e61d879b44229c5b62c27598010e6a757
harshit982 <sonkarharshit698@gmail.com>
2023-05-10 17:50:19 +0530

    added config

---
+++ firebaseconfig.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBre6iLJEGv2Hryfpv1Db-Szhe453izg70",
+  authDomain: "react-native-840d1.firebaseapp.com",
+  projectId: "react-native-840d1",
+  storageBucket: "react-native-840d1.appspot.com",
+  messagingSenderId: "577811237991",
+  appId: "1:577811237991:web:17f3c81d79a994a14d18c2",
+  measurementId: "G-HYGE38ZMMC"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
