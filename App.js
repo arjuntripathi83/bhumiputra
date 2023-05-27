c6a301720b85973f46f9b51a9042912281dcd16e
m-triple-m <triplem656@gmail.com>
2023-05-27 16:45:11 +0530

    ..

--- App.js
+++ App.js
@@ -23,52 +23,63 @@ export default function App() {
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <Profile visible={profileOpen} setVisible={setProfileOpen} />
+      
       <UserProvider>
-      <NavigationContainer>
-        <Tab.Navigator
-          screenOptions={({ route }) => ({
-            tabBarIcon: ({ focused, size }) => {
-              let iconName;
-              let color;
-              color = focused ? 'gray' : 'black';
-              size = focused ? 32 : 24;
-              if (route.name === 'Home') {
-                iconName = focused ? 'home' : 'home-outline';
-              } else if (route.name === 'EquipmentBrowser') {
-                return <Image source={require('./assets/icons/equipment.png')} style={{ width: 24, height: 24 }} />;
-                iconName = focused ? 'cart-sharp' : 'cart-outline';
-              } else if (route.name === 'FertilizerBrowser') {
-                iconName = focused ? 'cart-sharp' : 'cart-outline';
-              } else if (route.name === 'Seedsbrowser') {
-                iconName = focused ? 'nutrition' : 'nutrition-outline';
-                // return <Image source={require('./assets/icons/equipment.png')} style={{width: 24, height: 24}} />
-              } else if (route.name === 'Register') {
-                iconName = 'user';
-              }
+        <NavigationContainer>
+          <Tab.Navigator
+            screenOptions={({ route }) => ({
+              tabBarIcon: ({ focused, size }) => {
+                let iconName;
+                let color;
+                color = focused ? 'gray' : 'black';
+                size = focused ? 32 : 24;
+                if (route.name === 'Home') {
+                  iconName = focused ? 'home' : 'home-outline';
+                } else if (route.name === 'EquipmentBrowser') {
+                  return <Image source={require('./assets/icons/equipment.png')} style={{ width: 24, height: 24 }} />;
+                  iconName = focused ? 'cart-sharp' : 'cart-outline';
+                } else if (route.name === 'FertilizerBrowser') {
+                  iconName = focused ? 'cart-sharp' : 'cart-outline';
+                } else if (route.name === 'Seedsbrowser') {
+                  iconName = focused ? 'nutrition' : 'nutrition-outline';
+                  // return <Image source={require('./assets/icons/equipment.png')} style={{width: 24, height: 24}} />
+                } else if (route.name === 'Register') {
+                  iconName = 'user';
+                }
 
-              return <IonicIcon name={iconName} size={size} color={color} />;
-            }
-          })}
-        >
-          <Tab.Screen name="Home" children={() => <Home />} />
-          <Tab.Screen
-            name="EquipmentBrowser"
-            children={() => <Equipmentsbrowser />}
-            options={{
-              headerRight: (props) => <TouchableOpacity onPress={e => setProfileOpen(true)}>
-              <Image on style={{
-                height: 40,
-                width: 40,
-                borderRadius: 20,
-                marginRight: 10
-              }} source={{uri : 'https://creazilla-store.fra1.digitaloceanspaces.com/icons/7911322/avatar-icon-sm.png'}} />
-              </TouchableOpacity>
-            }}
-          />
-          <Tab.Screen name="FertilizerBrowser" children={() => <Fertilizersbrowser />} />
-          <Tab.Screen name="Seedsbrowser" children={() => <Seedsbrowser />} />
-        </Tab.Navigator>
-      </NavigationContainer>
+                return <IonicIcon name={iconName} size={size} color={color} />;
+              }
+            })}
+          >
+            <Tab.Screen name="Home" options={{
+              headerRight: (props) => (
+                <IconButton></IconButton>
+              )
+            }} children={() => <Home />} />
+            <Tab.Screen
+              name="EquipmentBrowser"
+              children={() => <Equipmentsbrowser />}
+              options={{
+                headerRight: (props) => (
+                  <TouchableOpacity onPress={(e) => setProfileOpen(true)}>
+                    <Image
+                      on
+                      style={{
+                        height: 40,
+                        width: 40,
+                        borderRadius: 20,
+                        marginRight: 10
+                      }}
+                      source={{ uri: 'https://creazilla-store.fra1.digitaloceanspaces.com/icons/7911322/avatar-icon-sm.png' }}
+                    />
+                  </TouchableOpacity>
+                )
+              }}
+            />
+            <Tab.Screen name="FertilizerBrowser" children={() => <Fertilizersbrowser />} />
+            <Tab.Screen name="Seedsbrowser" children={() => <Seedsbrowser />} />
+          </Tab.Navigator>
+        </NavigationContainer>
       </UserProvider>
     </SafeAreaView>
   );
