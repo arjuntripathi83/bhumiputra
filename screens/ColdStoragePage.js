16671c930553f7b14e32012d85ec49c716114d31
arjuntripathi83 <arjuntripathi83@gmail.com>
2023-06-17 15:27:45 -0400

    hjhvh

--- screens/ColdStoragePage.js
+++ screens/ColdStoragePage.js
@@ -1,16 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, StyleSheet } from 'react-native';
 import * as Location from 'expo-location';
 
-// Replace 'API_KEY' with your own Google Maps API key
-const API_KEY = 'API_KEY';
+const API_KEY = 'YOUR_API_KEY';
 
 const ColdStoragePage = () => {
   const [location, setLocation] = useState(null);
   const [coldStorages, setColdStorages] = useState([]);
 
   useEffect(() => {
-    // Request permission to access the user's location
     (async () => {
       let { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== 'granted') {
@@ -18,7 +16,6 @@ const ColdStoragePage = () => {
         return;
       }
 
-      // Get the current location
       let { coords } = await Location.getCurrentPositionAsync({});
       setLocation(coords);
     })();
@@ -26,18 +23,17 @@ const ColdStoragePage = () => {
 
   useEffect(() => {
     if (location) {
-      // Fetch nearby cold storages based on user's location
       fetchColdStorages();
     }
   }, [location]);
 
   const fetchColdStorages = async () => {
     const { latitude, longitude } = location;
-    const radius = 1000; // Specify the radius (in meters) for nearby search
+    const radius = 1000;
 
     try {
       const response = await fetch(
-        `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&radius=${radius}&keyword=cold storage&key=${API_KEY}`
+        `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&radius=${radius}&keyword=cold+storage&key=${API_KEY}`
       );
       const data = await response.json();
       setColdStorages(data.results);
@@ -48,30 +44,68 @@ const ColdStoragePage = () => {
 
   const renderColdStorages = () => {
     if (coldStorages.length === 0) {
-      return <Text>No cold storages found nearby.</Text>;
+      return <Text style={styles.noResultsText}>No cold storages found nearby.</Text>;
     }
 
     return coldStorages.map((coldStorage, index) => (
-      <View key={index}>
-        <Text>{coldStorage.name}</Text>
-        <Text>{coldStorage.vicinity}</Text>
-        <Text>{coldStorage.opening_hours?.open_now ? 'Open' : 'Closed'}</Text>
-        <Button title="Check Availability" onPress={() => handleAvailability(coldStorage)} />
+      <View key={index} style={styles.coldStorageContainer}>
+        <Text style={styles.coldStorageName}>{coldStorage.name}</Text>
+        <Text style={styles.coldStorageAddress}>{coldStorage.vicinity}</Text>
+        <Text style={styles.coldStorageStatus}>
+          {coldStorage.opening_hours?.open_now ? 'Open' : 'Closed'}
+        </Text>
+        <Button
+          title="Check Availability"
+          onPress={() => handleAvailability(coldStorage)}
+        />
       </View>
     ));
   };
 
   const handleAvailability = (coldStorage) => {
-    // Implement your logic to handle the availability feature
     console.log(`Checking availability for ${coldStorage.name}`);
   };
 
   return (
-    <View>
-      <Text>Nearby Cold Storages</Text>
+    <View style={styles.container}>
+      <Text style={styles.heading}>Nearby Cold Storages</Text>
       {renderColdStorages()}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: '#fff',
+  },
+  heading: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 16,
+  },
+  coldStorageContainer: {
+    marginBottom: 16,
+    padding: 16,
+    backgroundColor: '#f0f0f0',
+    borderRadius: 8,
+  },
+  coldStorageName: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 8,
+  },
+  coldStorageAddress: {
+    marginBottom: 8,
+  },
+  coldStorageStatus: {
+    marginBottom: 8,
+  },
+  noResultsText: {
+    textAlign: 'center',
+    marginTop: 16,
+  },
+});
 
 export default ColdStoragePage;
